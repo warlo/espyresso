d804b08b8644301be105113c95346a4258660fd2
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2018-10-14 16:39:48 +0200

    Add boiling variable

--- boiler.py
+++ boiler.py
@@ -3,10 +3,19 @@ from pwm import PWM
 
 
 class Boiler:
-    def __init__(self, pi, pwm_gpio):
+    def __init__(self, pi, pwm_gpio, boiling=True):
         self.pwm = PWM(pi, pwm_gpio)
+        self.boiling = boiling
+
+    def toggle_boiler(self):
+        self.boiling = not self.boiling
+        if not self.boiling:
+            self.pwm.set_value(0)
 
     def set_value(self, value):
+        if not self.boiling:
+            return
+
         if value < 0.0:
             value = 0.0
         elif value > 1.0:
