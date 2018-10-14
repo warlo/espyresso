7f70a4d91a20e22d5cb413bcf2cf48216cdaf4b8
Hans-Wilhelm Warlo <5417271+hanswilw@users.noreply.github.com>
2018-10-14 16:40:34 +0200

    Add initial buttons logic

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ from tsic import TsicInputChannel, Measurement
 from pid import PID
 from boiler import Boiler
 from display import Display
+from buttons import Buttons
 
 DEBUG = False
 
 TSIC_GPIO = 24
 PWM_GPIO = 13
+BUTTON_ONE_GPIO = 20
+BUTTON_TWO_GPIO = 21
 
 TARGET_TEMP = 95.0
 KP = 0.075
@@ -40,10 +43,12 @@ class Espyresso:
 
         self.boiler = Boiler(self.gpio, PWM_GPIO)
         self.tsic = TsicInputChannel(pigpio_pi=self.gpio, gpio=TSIC_GPIO)
+        self.buttons = Buttons(self.gpio, self.boiler, BUTTON_ONE_GPIO, BUTTON_TWO_GPIO)
+
         self.temp = 0
         self.running = True
 
-    def update(self):
+    def run(self):
         with self.tsic:
             while self.running:
                 time.sleep(0.2)
@@ -51,13 +56,14 @@ class Espyresso:
                 if latest_temp == Measurement.UNDEF:
                     if DEBUG:
                         print("UNDEF TEMP!")
-                else:
-                    self.temp = latest_temp.degree_celsius
-                    pid_value = self.pid.update(TARGET_TEMP - self.temp, self.temp)
-                    self.boiler.set_value(pid_value)
-                    self.display.draw(self.temp)
-                    if DEBUG:
-                        print(f"Temp: {round(self.temp, 2)} - PID: {pid_value}")
+                        continue
+
+                self.temp = latest_temp.degree_celsius
+                pid_value = self.pid.update(TARGET_TEMP - self.temp, self.temp)
+                self.boiler.set_value(pid_value)
+                self.display.draw(self.temp)
+                if DEBUG:
+                    print(f"Temp: {round(self.temp, 2)} - PID: {pid_value}")
 
     def signal_handler(self, sig, frame):
         if sig == 2:
@@ -74,7 +80,7 @@ class Espyresso:
 
 def handler(signum, frame):
     """Why is systemd sending sighups? I DON'T KNOW."""
-    logging.warning("Got a {} signal. Doing nothing".format(signum))
+    logging.warning(f"Got a {signum} signal. Doing nothing")
 
 
 if __name__ == "__main__":
@@ -84,6 +90,6 @@ if __name__ == "__main__":
         espyresso = Espyresso()
         signal.signal(signal.SIGINT, espyresso.signal_handler)
         signal.signal(signal.SIGTERM, espyresso.signal_handler)
-        espyresso.update()
+        espyresso.run()
     except Exception as e:
         logging.warning(f"EXCEPTION:{e}")
