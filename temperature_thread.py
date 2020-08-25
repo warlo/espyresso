3408006e4641b20ae7f257cf00207c9c64312e27
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2020-08-25 17:46:23 +0200

    Fix started time

--- temperature_thread.py
+++ temperature_thread.py
@@ -13,9 +13,9 @@ from tsic import Measurement, TsicInputChannel
 
 class TemperatureThread(threading.Thread):
     def __init__(
-        self, *args, pigpio_pi=None, boiler=None, display=None, pid=None, **kwargs
+        self, *args, pigpio_pi=None, boiler=None, display=None, pid=None, started_time=None **kwargs
     ):
-        self.started_time = 0
+        self.started_time = started_time
 
         self.boiler = boiler
         self.display = display
