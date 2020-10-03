db8fb116ea2a698bb96b7b049a2aaa8bf49ee99f
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2020-10-03 10:08:07 +0200

    Try other pulse logic with queue

--- flow.py
+++ flow.py
@@ -5,25 +5,27 @@ import pigpio
 
 
 class Flow:
-    def __init__(self, pigpio_pi=None, flow_in_gpio=None):
+    def __init__(self, pigpio_pi=None, flow_in_gpio=None, add_to_queue=None):
         self.pigpio_pi = pigpio_pi
         self.flow_in_gpio = flow_in_gpio
         self.pigpio_pi.set_mode(self.flow_in_gpio, pigpio.INPUT)
-        self.callback_flow = self.pigpio_pi.callback(
-            self.flow_in_gpio, pigpio.RISING_EDGE
+        self.pigpio_pi.callback(
+            self.flow_in_gpio, pigpio.RISING_EDGE, self.pulse_callback
         )
 
         self.flowing = False
+        self.add_to_queue = add_to_queue
 
         self.pulses_since = time.time()
         self.pulse_count = 0
 
     def reset_pulse_count(self):
-        self.callback_flow.reset_tally()
+        self.pulse_count = 0
         self.pulses_since = time.time()
 
-    def get_pulse_count(self):
-        return self.callback_flow.tally()
+    def pulse_callback(self, gpio, level, tick):
+        self.pulse_count += tick
+        self.add_to_queue(self.get_millilitres_per_sec())
 
     def get_litres(self):
         counts_per_liter = 1925
