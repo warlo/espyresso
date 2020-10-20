faf61d8757a23a06daceb3e884292ce20d067f1b
Hans-Wilhelm Warlo <hanswilw@users.noreply.github.com>
2020-10-20 19:09:04 +0200

    Flow per second improvement

--- flow.py
+++ flow.py
@@ -13,15 +13,21 @@ class Flow:
             self.flow_in_gpio, pigpio.RISING_EDGE, self.pulse_callback
         )
 
+        self.counts_per_liter = 3000
+        #self.counts_per_liter = 1925
+
         self.flowing = False
         self.add_to_queue = add_to_queue
 
-        self.pulses_since = time.time()
+        self.pulse_start = time.time()
         self.pulse_count = 0
+
+        self.last_time = time.time()
+        self.last_pulse_count = 0
 
     def reset_pulse_count(self):
         self.pulse_count = 0
-        self.pulses_since = time.time()
+        self.pulses_start = time.time()
 
     def pulse_callback(self, gpio, level, tick):
         self.pulse_count += 1
@@ -31,12 +37,24 @@ class Flow:
         return self.pulse_count
 
     def get_litres(self):
-        counts_per_liter = 1925
-        return self.get_pulse_count() / counts_per_liter
+        return self.get_pulse_count() / self.counts_per_liter
+
+    def get_litres_diff(self):
+        pulse_count = self.get_pulse_count()
+        pulse_diff = pulse_count - self.last_pulse_count
+        self.last_pulse_count = pulse_count 
+
+        return pulse_diff / self.counts_per_liter
 
     def get_millilitres(self):
         return self.get_litres() * 1000
 
+    def get_millilitres_diff(self):
+        return self.get_litres_diff() * 1000
+
     def get_millilitres_per_sec(self):
-        time_since_started = time.time() - self.pulses_since
-        return self.get_millilitres() / time_since_started
+        current_time = time.time()
+        time_diff = current_time - self.last_time
+        self.last_time = current_time
+
+        return self.get_millilitres_diff() / time_diff
