3a75da6507829e6fb0cbea0d323fb210880f7665
Hans-Wilhelm Warlo <5417271+hanswilw@users.noreply.github.com>
2021-09-01 12:01:55 +0200

    Add timer class

---
+++ timer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import time
+
+
+class Timer:
+    def __init__(
+        self,
+    ):
+        self.started = None
+        self.stopped = None
+
+    def get_time_since_started(self):
+        if self.stopped and self.started:
+            return self.stopped - self.started
+        if self.started:
+            return time.time() - self.started
+        return 0
+
+    def start_timer(self):
+        self.stopped = None
+        self.started = time.time()
+
+    def stop_timer(self, *, subtract_time=0):
+        self.stopped = time.time() - subtract_time
+
+    def reset_timer(self):
+        self.stopped = None
+        self.stopped = None
