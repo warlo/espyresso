eb5640b68147c4207969077490e77ee0576ffb59
Hans-Wilhelm Warlo <5417271+hanswilw@users.noreply.github.com>
2021-09-01 12:49:52 +0200

    Add stopped clause

--- timer.py
+++ timer.py
@@ -47,6 +47,11 @@ class BrewingTimer(threading.Thread):
             return time.time() - self.started
         return 0
 
+    def get_time_since_stopped(self):
+        if self.stopped:
+            return time.time() - self.stopped
+        return 0
+
     def timer_running(self):
         return self.started and not self.stopped
 
@@ -67,7 +72,7 @@ class BrewingTimer(threading.Thread):
     def run(self):
         while self.running:
 
-            if not self.timer_running() and self.flow.get_time_since_last_pulse() < 1:
+            if not self.timer_running() and self.get_time_since_stopped() > 1 and self.flow.get_time_since_last_pulse() < 1:
                 self.start_timer()
 
             elif self.timer_running() and self.flow.get_time_since_last_pulse() > 1:
