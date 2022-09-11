ee7f8b37ae226cd4622ac20128bb7d465cbd5c98
Hans-Wilhelm Warlo <5417271+warlo@users.noreply.github.com>
2022-09-11 18:27:40 +0200

    Increase min pwm on gradual reduce

--- espyresso/pump.py
+++ espyresso/pump.py
@@ -176,7 +176,7 @@ class Pump:
 
             # Gradually reduce pump pressure before end
             if current_grams > (target_grams - 5):
-                self.set_pwm_value(min(0.3, (target_grams - current_grams) / 5))
+                self.set_pwm_value(min(0.5, (target_grams - current_grams) / 5))
 
             time_passed = self.brewing_timer.get_time_since_started()
 
